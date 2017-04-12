465421d286c4b6349b3f8665f8c773eb297bf336
Werter <shai.halimi@gmail.com>
2017-04-12 16:27:11 +0200

    Add Quake player

--- src/main/java/ch/werter/Quake.java
+++ src/main/java/ch/werter/Quake.java
@@ -1,7 +1,28 @@
 package ch.werter;
 
+import ch.werter.listener.PlayerEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+
 /**
  * Created by werter on 12.04.2017.
  */
-public class Quake {
+public class Quake extends JavaPlugin {
+
+    private HashMap<Player,QuakePlayer> quakePlayers = new HashMap<Player, QuakePlayer>();
+
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(new PlayerEvent(this),this);
+    }
+
+    public void registerPlayer(Player player){
+        this.quakePlayers.put(player,new QuakePlayer());
+    }
+
+    public QuakePlayer getQuakePlayer(Player player){
+        return  this.quakePlayers.get(player);
+    }
 }
